06eefb1712e5cb5e761648b0228a015326869738
zblkartik <kartikshikhare26@gmail.com>
2024-04-28 15:34:07 +0530

    Fixed product Data

--- public/data/productsData.js
+++ public/data/productsData.js
@@ -3,43 +3,44 @@ const products = [
         name: "LT PVC POWER & CONTROL CABLES",
         category: "LT Cables",
         identifier: "LT-PVC-POWER-CONTROL-CABLES",
-        description: [{
-            title: "Standards",
-            content: ["IS-1554 (Part -1) 1988"]
-        },
-        {
-            title: "Conductor",
-            content: ["The most acceptable metals for wires & cables are copper and aluminium due to their higher conductivity & ductility. Copper has higher affinity for sulphur it corrodes in the atmosphere where sulphur fumes is present. In these conditions tinned copper should be used as it forms a protective film over it & prevents it from tarnishing. Aluminium oxide film present on the surface of aluminium conductor acts as a barrier to protect it from corrosion. CONDUCTOR CONSTRUCTION The most economical construction for conductor is solid conductor i.e. conductor made of one single wire. As the area of conductor increases, solid conductor becomes stiffer and difficult to handle, therefore stranded construction is adopted. Here the conductor is made of number of strands. The strands are arranged in spiral layers in 1+6+12+18+…… formations. This geometrical progression of strands provides more compactness & flexibility. The stranded construction of conductors is more suitable for crimping of lugs. To economize the weight of insulating material, to optimize overall diameter, conductors are shaped & compacted in higher sized cables. The stranded conductor is shaped in to a segment of a circle so that when all the cores are laid, they form a complete circle. These segments are identified as 2 Core- 180 degrees, 3 Core- 120 degrees, 4 Core- 90 degrees, 3.5 Core- 100/60 degrees. IS 1554 permits solid conductor construction up to 10 sqmm in aluminium and up to 6 sqmm in copper. It permits the use of shaped conductors for size from 16 sqmm onwards."]
-        },
-        {
-            title: "Insulation",
-            description: ["The PVC covering over conductor is called insulation and it is provided by extrusion process only. The insulated conductor is called a core."],
-            bullets: [
-                "Insulation with type A PVC Compound as per IS: 5831, suitable for 70°C continuous operation."
-                , "Insulation with type C PVC Compound as per IS: 5831, suitable for 85°C continuous operation."
-            ]
-        },
-        {
-            title: "Laying Up",
-            content: ["The cores are laid up with suitable lay. The final layer always has a right hand lay i.e. if you look along the cable, the cores move to your right hand or clock wise."]
-        },
-        {
-            title: "Inner Sheath",
-            content: ["Inner sheath is provided over the laid-up cores. It is provided to give circular shape to the cable and it provides bedding for the armouring. I.S. 1554 permits following two methods of applying the inner sheath of any thermoplastic material i.e. PVC."],
-            bullets: [
-                "Extruded Inner Sheath: It is provided by extrusion of thermoplastic material over the laid-up cores. This type of inner sheath also acts as a water barrier between cores and outer sheath. Is case of a puncture in the outer sheath water cannot reach to the cores and hence we recommend that cables for outdoor underground uses should have extruded inner sheath.",
-                "Taped Inner Sheath: It is provided by wrapping a thermoplastic tape over the laid-up cores. This method saves a process and hence manufactures always provide this type of inner sheath unless the purchase specifications ask for extruded inner sheath."
-            ]
-        },
-        {
-            title: "Armouring",
-            content: ["Cable armouring consists of galvanized steel wire / strip provided over the inner sheath in multi core cables and aluminium round wire / strip over the insulation in single core cables. It provides mechanical protection to the cable & carries earth fault current in case of short circuit of a core with armour.", "As per IS:1554 (Part-1) 1988, round wire armouring is provided in cable, where calculated diameter under armour is upto 13 mm. Above this the armouring is either with round wire or strip of size 4mm x 0.80mm. In long run of cables and in case of mines, round wire armouring is a must, as strip construction provide higher resistance to earth fault current and sometimes this current may not be sufficient to operate the circuit breaker in case of earth fault. In mines, the resistance of the armour in no case should exceed the resistance of the main core by more than 33% for safety reasons. To achieve this, sometimes tinned hard drawn copper wires are used along with galvanized steel wires. Sometimes two layers of round steel wire or steel strip are provided in opposite direction with barrier tape in between to provide extra protection. In case of single core cable for use in AC circuits, the material for armouring has to be non magnetic, as in this case the return current is not passing through the same cable and hence it will not cancel the magnetic field produced by the current. These magnetic lines which are oscillating in case of AC current will give rise to eddy current in magnetic armouring and hence armouring will become hot and this may lead to the failure of the cable. Generally hard drawn aluminium wires / strip are used for armouring in this case."
-            ]
-        }, {
-            title: "Outer Sheath",
-            content: ["The PVC covering over the armouring in case of armoured cables and over the inner sheath in case of un-armoured cables is called outer sheath. IS: 1554 specifies nominal and minimum thickness of outer sheath for unarmoured cables and only minimum thickness of outer sheath for armoured cables. Types of PVC compounds for outer sheath.", "1. Outer sheath with type ST1 PVC compound as per IS: 5831, which is suitable for 70°C continuous operation. ", "2.Outer sheath with type ST2 PVC compound as per IS: 5831, which is suitable for 85°C continuous operation.", "Currently in Power, Chemical, Fertilizer and Cement plants many PVC cables are placed in the cable shaft or on cable trays. In case of fire burning of this huge bunch of PVC cables, emits a dense corrosive smoke. Due to thick black smoke and toxic HCL content of the smoke visibility is impaired to a large extent & it also creates suffocation which totally defeats firefighting.", "To overcome these deficiencies cables were developed with FR / FRLSH / LSZH properties. We are capable of providing cables with any of these properties."
-            ]
-        }
+        description: [
+            {
+                title: "Standards",
+                content: ["IS-1554 (Part -1) 1988"]
+            },
+            {
+                title: "Conductor",
+                content: ["The most acceptable metals for wires & cables are copper and aluminium due to their higher conductivity & ductility. Copper has higher affinity for sulphur it corrodes in the atmosphere where sulphur fumes is present. In these conditions tinned copper should be used as it forms a protective film over it & prevents it from tarnishing. Aluminium oxide film present on the surface of aluminium conductor acts as a barrier to protect it from corrosion. CONDUCTOR CONSTRUCTION The most economical construction for conductor is solid conductor i.e. conductor made of one single wire. As the area of conductor increases, solid conductor becomes stiffer and difficult to handle, therefore stranded construction is adopted. Here the conductor is made of number of strands. The strands are arranged in spiral layers in 1+6+12+18+…… formations. This geometrical progression of strands provides more compactness & flexibility. The stranded construction of conductors is more suitable for crimping of lugs. To economize the weight of insulating material, to optimize overall diameter, conductors are shaped & compacted in higher sized cables. The stranded conductor is shaped in to a segment of a circle so that when all the cores are laid, they form a complete circle. These segments are identified as 2 Core- 180 degrees, 3 Core- 120 degrees, 4 Core- 90 degrees, 3.5 Core- 100/60 degrees. IS 1554 permits solid conductor construction up to 10 sqmm in aluminium and up to 6 sqmm in copper. It permits the use of shaped conductors for size from 16 sqmm onwards."]
+            },
+            {
+                title: "Insulation",
+                description: ["The PVC covering over conductor is called insulation and it is provided by extrusion process only. The insulated conductor is called a core."],
+                bullets: [
+                    "Insulation with type A PVC Compound as per IS: 5831, suitable for 70°C continuous operation."
+                    , "Insulation with type C PVC Compound as per IS: 5831, suitable for 85°C continuous operation."
+                ]
+            },
+            {
+                title: "Laying Up",
+                content: ["The cores are laid up with suitable lay. The final layer always has a right hand lay i.e. if you look along the cable, the cores move to your right hand or clock wise."]
+            },
+            {
+                title: "Inner Sheath",
+                content: ["Inner sheath is provided over the laid-up cores. It is provided to give circular shape to the cable and it provides bedding for the armouring. I.S. 1554 permits following two methods of applying the inner sheath of any thermoplastic material i.e. PVC."],
+                bullets: [
+                    "Extruded Inner Sheath: It is provided by extrusion of thermoplastic material over the laid-up cores. This type of inner sheath also acts as a water barrier between cores and outer sheath. Is case of a puncture in the outer sheath water cannot reach to the cores and hence we recommend that cables for outdoor underground uses should have extruded inner sheath.",
+                    "Taped Inner Sheath: It is provided by wrapping a thermoplastic tape over the laid-up cores. This method saves a process and hence manufactures always provide this type of inner sheath unless the purchase specifications ask for extruded inner sheath."
+                ]
+            },
+            {
+                title: "Armouring",
+                content: ["Cable armouring consists of galvanized steel wire / strip provided over the inner sheath in multi core cables and aluminium round wire / strip over the insulation in single core cables. It provides mechanical protection to the cable & carries earth fault current in case of short circuit of a core with armour.", "As per IS:1554 (Part-1) 1988, round wire armouring is provided in cable, where calculated diameter under armour is upto 13 mm. Above this the armouring is either with round wire or strip of size 4mm x 0.80mm. In long run of cables and in case of mines, round wire armouring is a must, as strip construction provide higher resistance to earth fault current and sometimes this current may not be sufficient to operate the circuit breaker in case of earth fault. In mines, the resistance of the armour in no case should exceed the resistance of the main core by more than 33% for safety reasons. To achieve this, sometimes tinned hard drawn copper wires are used along with galvanized steel wires. Sometimes two layers of round steel wire or steel strip are provided in opposite direction with barrier tape in between to provide extra protection. In case of single core cable for use in AC circuits, the material for armouring has to be non magnetic, as in this case the return current is not passing through the same cable and hence it will not cancel the magnetic field produced by the current. These magnetic lines which are oscillating in case of AC current will give rise to eddy current in magnetic armouring and hence armouring will become hot and this may lead to the failure of the cable. Generally hard drawn aluminium wires / strip are used for armouring in this case."
+                ]
+            }, {
+                title: "Outer Sheath",
+                content: ["The PVC covering over the armouring in case of armoured cables and over the inner sheath in case of un-armoured cables is called outer sheath. IS: 1554 specifies nominal and minimum thickness of outer sheath for unarmoured cables and only minimum thickness of outer sheath for armoured cables. Types of PVC compounds for outer sheath.", "1. Outer sheath with type ST1 PVC compound as per IS: 5831, which is suitable for 70°C continuous operation. ", "2.Outer sheath with type ST2 PVC compound as per IS: 5831, which is suitable for 85°C continuous operation.", "Currently in Power, Chemical, Fertilizer and Cement plants many PVC cables are placed in the cable shaft or on cable trays. In case of fire burning of this huge bunch of PVC cables, emits a dense corrosive smoke. Due to thick black smoke and toxic HCL content of the smoke visibility is impaired to a large extent & it also creates suffocation which totally defeats firefighting.", "To overcome these deficiencies cables were developed with FR / FRLSH / LSZH properties. We are capable of providing cables with any of these properties."
+                ]
+            }
 
         ]
     },
--- src/app/products/[product]/page.js
+++ src/app/products/[product]/page.js
@@ -5,8 +5,14 @@ import products from '../../../../public/data/productsData'
 
 export async function getPageData(product) {
   const id = product// Get ID from slug `/book/1` 
-  return products.find((ele) => ele.identifier == id);
-  // Rest of `getServerSideProps` code
+  const data = products.find((ele) => ele.identifier == id) || undefined;
+  return new Promise((resolve, reject) => {
+    if (data) {
+      resolve(data)
+    } else {
+      reject()
+    }
+  })
 }
 export default async function page({ params: { product } }) {
   const productData = await getPageData(product)
